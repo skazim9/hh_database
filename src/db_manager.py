163d99841b0d828ead1fc887703df9f856b967e0
MaksimVolkov <skazimko@yandex.ru>
2024-10-13 22:07:58 +0300

    update classes HHapi and DBManager, add main.py

--- src/db_manager.py
+++ src/db_manager.py
@@ -1,42 +1,96 @@
+from decimal import Decimal
+
 import psycopg2
 
 
 class DBManager:
-    """Класс-менеджер для работы с БД PostgreSQL."""
+    """Класс для работы с БД"""
 
-    def __init__(self, host="localhost", user="postgres", password="Q1980qum%", database="postgres", port="5432"):
-        self.__connection_params = {
-            "host": host,
-            "user": user,
-            "password": password,
-            "database": database,
-            "port": port,
-        }
-
-    def __execute_query(self, query):
-        try:
-            with psycopg2.connect(**self.__connection_params) as conn:
-                with conn.cursor() as cursor:
-                    cursor.execute(query)
-                    return cursor.fetchall() if query.strip().lower().startswith("select") else None
-        except Exception as e:
-            print(f"Ошибка при выполнении запроса: {e}")
+    def __init__(self, params):
+        self.conn = psycopg2.connect(dbname='hh_db', **params)
+        self.cur = self.conn.cursor()
 
     def get_companies_and_vacancies_count(self):
-        """Метод получения списка всех компаний и количества вакансий у каждой компании."""
-        pass
+        """получает список всех компаний и количество вакансий у каждой компании."""
+        self.cur.execute("""
+                    SELECT employer_name, COUNT(vacancies.employer_id)
+                    FROM employers
+                    INNER JOIN vacancies USING (employer_id)
+                    GROUP BY employer_name
+                    ORDER BY COUNT DESC
+            """)
+
+        return self.cur.fetchall()
 
     def get_all_vacancies(self):
-        """Метод получения списка всех вакансий с указанием названия компании,
-        названия вакансии, зарплаты и ссылки на вакансию."""
-        pass
+        """список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию."""
+        self.cur.execute("""
+                    SELECT e.employer_name, v.vacancy_name, v.salary, v.vacancy_url
+                    FROM vacancies v
+                    INNER JOIN employers e USING (employer_id)
+                    WHERE v.salary IS NOT NULL AND v.salary != 0
+                    ORDER BY v.salary DESC
+
+            """)
+
+        return self.cur.fetchall()
+
     def get_avg_salary(self):
-        """Метод получения средней зарплаты по вакансиям."""
-        pass
+        """получает среднюю зарплату по вакансиям."""
+        self.cur.execute("""
+                    SELECT AVG(salary)
+                    FROM vacancies
+            """)
+
+        result = self.cur.fetchone()
+        avg_salary = Decimal(result[0])
+        formatted_avg_salary = format(avg_salary, '.2f')
+        return formatted_avg_salary
 
     def get_vacancies_with_higher_salary(self):
-        """Метод получения списка всех вакансий, у которых зарплата выше средней по всем вакансиям."""
-        pass
-    def get_vacancies_with_keyword(self, key_words):
-        """Метод получения списка всех вакансий, в названии которых содержатся переданные в метод слова"""
-        pass
+        """получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
+        avg_salary = self.get_avg_salary()[0][0]
+
+        self.cur.execute(
+            """
+            SELECT v.vacancy_name, v.salary
+            FROM vacancies v
+            WHERE v.salary > %s
+            """, (avg_salary,)
+        )
+        return self.cur.fetchall()
+
+    def get_vacancies_with_keyword(self, keyword):
+        """получает список всех вакансий, в названии которых содержатся переданные в метод слова."""
+        keyword = f"%{keyword.lower()}%"
+        self.cur.execute("""
+                    SELECT vacancy_name
+                    FROM vacancies
+                    WHERE vacancy_name LIKE %s
+            """, (keyword,))
+
+        return self.cur.fetchall()
+
+    def get_vacancies_with_average_salary(self):
+        """
+        Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
+        """
+
+        query = """
+        SELECT company_name, job_title, salary_from, salary_to, vacancy_url FROM vacancies
+        WHERE currency = 'RUR' and salary_from > (SELECT AVG(salary_from) FROM vacancies)
+        """
+        self.cur.execute(query)
+        return self.cur.fetchall()
+
+    def get_vacancies_by_word(self, keyword):
+        """
+        Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python.
+        """
+
+        query = """
+        SELECT * FROM vacancies
+        WHERE LOWER(job_title) LIKE %s
+        """
+        self.cur.execute(query, ("%" + keyword.lower() + "%",))
+        return self.cur.fetchall()
--- src/hh_api.py
+++ src/hh_api.py
@@ -1,11 +1,46 @@
 import requests
 
 
-class HHapi:
+class HH:
     """Класс работы с API hh"""
 
     def __init__(self):
         self.__url = "https://api.hh.ru"
         self._headers = {"User-Agent": "HH-User-Agent"}
         self._params = {"per_page": 100, "page": 0, "only_with_salary": True}
-        self.employers = []
+        self.employers = [
+            3529,
+            127256,
+            80,
+            9694561,
+            4219,
+            9301808,
+            10571093,
+            78638,
+            4934,
+            64174
+        ]
+
+    def get_employers(self):
+        """Загрузка работодателей"""
+        list_employers = []
+        for employer_id in self.employers:
+            temp_url = f"{self.__url}/employers/{employer_id}"
+            employer_data = requests.get(temp_url).json()
+            list_employers.append(employer_data)
+
+        return list_employers
+
+    def load_vacancies(self):
+        """Загрузка вакансий"""
+        list_vacancy = []
+        for employer_id in self.employers:
+            self._params["employer_id"] = employer_id
+            vacancy_url = f"{self.__url}/vacancies"
+            response = requests.get(
+                vacancy_url, headers=self._headers, params=self._params
+            )
+            vacancies = response.json()["items"]
+            list_vacancy.extend(vacancies)
+
+        return list_vacancy
